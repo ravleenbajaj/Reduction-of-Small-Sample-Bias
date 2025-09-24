1acfdfc47d041a05a7b92490bf2d520313f1e0aa
Ravleen Bajaj <96468459+ravleenbajaj@users.noreply.github.com>
2025-09-24 11:03:12 -0700

    Update firth_example.R

--- examples/firth_example.R
+++ examples/firth_example.R
@@ -1,23 +1,6 @@
 library(brglm2)
 set.seed(2025)
 
-generate_data <- function(n, beta_0, beta_1){
-X1 <- rnorm(n, mean = 0, sd = 2)
-X <- cbind(1, X1)
-beta_true <- c(beta_0, beta_1)
-eta <- X %*% beta_true
-p <- 1 / (1 + exp(-eta))
-y <- rbinom(n, size = 1, prob = p)
-df <- data.frame(
-y = y,
-X1 = X1
-)
-  
-return(df)
-}
-data <- generate_data(50, 0.5, -0.3)
-
-head(data)
 
 fit_MLE <- glm(y ~ X1, family=binomial(link="logit"), data=data)
 summary(fit_MLE)
@@ -25,6 +8,7 @@ summary(fit_MLE)
 fit_Firth <- glm(y ~ X1, family=binomial(link="logit"), data=data,
 method=brglmFit, type="AS_mean")
 summary(fit_Firth
+
 
 
 
